be2c22b9617d353a96b8722072b22e4d2756b9cb
withlzc <withlzc@gmail.com>
2019-07-11 09:23:55 +0800

    first commit

---
+++ admin/src/router/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HelloWorld
+    },
+    {
+      path: '/',
+      name: 'login',
+      component: Login
+      // meta: {
+      //   isLogin: false
+      // }
+    }
+  ]
+})
