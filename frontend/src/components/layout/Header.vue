b45bb19c575bae6c2f700edd9e0dc38f91cb04b6
withlzc <withlzc@gmail.com>
2019-07-15 15:22:04 +0800

    update header

--- frontend/src/components/layout/Header.vue
+++ frontend/src/components/layout/Header.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="m-header m-img">
-    <div class="m-heading">
-      <h1>My Blog</h1>
-      <span class="subheading">Where there's a will, there's a way.</span>
+  <header class="m-header">
+    <div class="container">
+      <div class="m-heading">
+        <h1>My Blog</h1>
+        <span class="subheading">Where there's a will, there's a way.</span>
+      </div>
     </div>
-  </div>
+  </header>
 </template>
 
 <script>
@@ -17,24 +19,18 @@
   .m-header {
     background: #ccc url("../../assets/img/home-bg.jpg") scroll;
     background-size: cover;
-    padding: 0 50px;
-    width: 1200px;
-    margin-left: -28px;
+    /*padding: 0 20px;*/
+    /*width: 1400px;*/
+    margin-left: -8px;
+    margin-right: -10px;
     margin-top: -68px;
   }
-
-  .m-img {
-    max-width: 100%;
-    height: auto;
-  }
-
   .m-heading {
     padding: 50px 0;
     color: white;
     margin-top: 0px;
     font-size: 50px;
   }
-
   .subheading {
     font-size: 18px;
     line-height: 1.1;
