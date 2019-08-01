17306f9e2808e8294b961c2e5e066f381094bc57
withlzc <withlzc@gmail.com>
2019-08-01 11:09:53 +0800

    update home page

--- frontend/src/views/blog/Home.vue
+++ frontend/src/views/blog/Home.vue
@@ -4,15 +4,17 @@
     <b-header></b-header>
     <br>
     <br>
-    <el-row :gutter="20">
-      <el-col :span="16">
-        <b-content></b-content>
-        <b-page/>
-      </el-col>
-      <el-col :span="8">
-        <b-sider></b-sider>
-      </el-col>
-    </el-row>
+    <div class="m-container">
+      <el-row :gutter="20">
+        <el-col :span="16">
+          <b-content></b-content>
+          <b-page/>
+        </el-col>
+        <el-col :span="8">
+          <b-sider></b-sider>
+        </el-col>
+      </el-row>
+    </div>
     <b-footer></b-footer>
   </div>
 </template>
@@ -44,5 +46,11 @@
 </script>
 
 <style scoped>
-
+  .m-container {
+    width: 1170px;
+    padding-right: 15px;
+    padding-left: 15px;
+    margin-right: auto;
+    margin-left: auto;
+  }
 </style>
