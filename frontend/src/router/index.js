6bff4c48f53c1cd392ab16a22efd91b6b524cb55
withlzc <withlzc@gmail.com>
2019-08-18 21:18:20 +0800

    update route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/views/blog/Home'
-import Archive from '@/views/blog/Archive'
-import Category from '@/views/blog/Category'
-import Tag from '@/views/blog/Tag'
-import Post from '@/views/blog/Post'
-import Login from '@/views/admin/Login'
+// import Home from '@/views/blog/Home'
+// import Archive from '@/views/blog/Archive'
+// import Category from '@/views/blog/Category'
+// import Tag from '@/views/blog/Tag'
+// import Post from '@/views/blog/Post'
+// import Login from '@/views/admin/Login'
 import Layout from '../views/admin/Layout'
-// import Newpost from '../views/admin/Newpost'
+import Newpost from '../views/admin/Newpost'
 
 Vue.use(Router)
 
@@ -15,34 +15,34 @@ export const constantRoutes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: () => import('@/views/blog/Home')
   },
   {
     path: '/archive',
     name: 'Archive',
-    component: Archive
+    component: () => import('@/views/blog/Archive')
   },
   {
     path: '/category',
     name: 'Category',
-    component: Category
+    component: () => import('@/views/blog/Category')
   },
   {
     path: '/tag',
     name: 'Tag',
-    component: Tag
+    component: () => import('@/views/blog/Tag')
   },
   {
     path: '/post',
     name: 'Post',
-    component: Post
+    component: () => import('@/views/blog/Post')
   },
 
   //Admin
   {
     path: '/admin/login',
     name: 'Login',
-    component: Login
+    component: () => import('@/views/admin/Login')
   },
   {
     path: '/admin',
@@ -55,20 +55,20 @@ export const constantRoutes = [
       component: () => import('@/views/admin/Dashboard'),
       meta: {title: 'Dashboard', icon: 'dashboard'}
     }, {
-      path: 'posts',
-      redirect: 'posts/all-posts',
-      name: 'Posts',
-      meta: {title: 'Posts', icon: 'posts'},
-    }, {
       path: 'posts/all-posts',
-      name: 'Tags',
+      name: 'AllPosts',
       component: () => import('@/views/admin/PostsList'),
-      meta: {title: 'Tag', icon: 'tag'}
+      meta: {title: 'AllPosts', icon: 'allPosts'}
+    }, {
+      path: 'posts/new-post',
+      name: 'NewPosts',
+      component: () => import('@/views/admin/Newpost'),
+      meta: {title: 'Newpost', icon: 'newpost'}
     }, {
       path: 'posts/categories',
-      name: 'Tags',
+      name: 'Categories',
       component: () => import('@/views/admin/Categories'),
-      meta: {title: 'Tag', icon: 'tag'}
+      meta: {title: 'Category', icon: 'category'}
     }, {
       path: 'posts/tags',
       name: 'Tags',
@@ -84,8 +84,7 @@ export const constantRoutes = [
       name: 'Comments',
       component: () => import('@/views/admin/Comments'),
       meta: {title: 'Comments', icon: 'comments'}
-    },
-    ]
+    }]
   },
 
 ]
@@ -99,9 +98,9 @@ const createRouter = () => new Router({
 const router = createRouter()
 
 // Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
-export function resetRouter () {
-  const newRouter = createRouter()
-  router.matcher = newRouter.matcher // reset router
-}
+// export function resetRouter () {
+//   const newRouter = createRouter()
+//   router.matcher = newRouter.matcher // reset router
+// }
 
 export default router
